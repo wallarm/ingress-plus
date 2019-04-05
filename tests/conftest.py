bdc42c892cef0bb6735a9921d86bcf0f39d94437
tellet <tellet.tat@gmail.com>
2019-04-05 17:35:33 +0100

    Add tests project documentation

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,22 +14,22 @@ def pytest_addoption(parser) -> None:
     :return:
     """
     parser.addoption("--context",
-                     action="store", default="", help="context name as in the kubeconfig")
+                     action="store", default="", help="The context to use in the kubeconfig file.")
     parser.addoption("--image",
-                     action="store", default=DEFAULT_IMAGE, help="image with tag (image:tag)")
+                     action="store", default=DEFAULT_IMAGE, help="The Ingress Controller image.")
     parser.addoption("--image-pull-policy",
-                     action="store", default=DEFAULT_PULL_POLICY, help="image pull policy")
+                     action="store", default=DEFAULT_PULL_POLICY, help="The pull policy of the Ingress Controller image.")
     parser.addoption("--ic-type",
-                     action="store", default=DEFAULT_IC_TYPE, help="provide ic type")
+                     action="store", default=DEFAULT_IC_TYPE, help="The type of the Ingress Controller: nginx-ingress or nginx-ingress-plus.")
     parser.addoption("--service",
                      action="store",
                      default=DEFAULT_SERVICE,
-                     help="service type: nodeport or loadbalancer")
-    parser.addoption("--node-ip", action="store", help="public IP of a cluster node")
+                     help="The type of the Ingress Controller service: nodeport or loadbalancer.")
+    parser.addoption("--node-ip", action="store", help="The public IP of a cluster node. Not required if you use the loadbalancer service (see --service argument).")
     parser.addoption("--kubeconfig",
                      action="store",
                      default=os.path.expanduser(KUBE_CONFIG_DEFAULT_LOCATION),
-                     help="an absolute path to kubeconfig")
+                     help="An absolute path to a kubeconfig file.")
 
 
 # import fixtures into pytest global namespace
